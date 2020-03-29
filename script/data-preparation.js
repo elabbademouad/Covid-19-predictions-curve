7bf1e14aaa03aa2bc07fc7dae4234b21bd212d62
Mouad El abbade <elabbademouad@gmail.com>
2020-03-29 18:21:00 +0100

    update data

--- script/data-preparation.js
+++ script/data-preparation.js
@@ -14,6 +14,6 @@ for (let i = 0; i < activeCases.length; i++) {
 var regresstionCoef=calculateRegressionCoef(cases);
 
 
-for (let i = 0; i < cases.length+7; i++) {
+for (let i = 0; i < cases.length+5; i++) {
     predections.push(Math.round(regresstionCoef.a+Math.pow(regresstionCoef.b,i)));   
 }
